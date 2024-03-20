084287a72cc89f8cc48891cfd00826386cdcb0d6
Renata Kimura <rkymura@hotmail.com>
2024-03-20 23:49:40 +0000

    Aula06Investimentos

---
+++ Aula06Investimentos/Form1.cs
@@ -0,0 +1,26 @@
+namespace Aula06Investimentos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var numero1 = int.Parse(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            var numero2 = int.Parse(textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var resultado = int.Parse(textBox1.Text) / int.Parse(textBox2.Text) * 100;
+            MessageBox.Show(resultado + " % ");
+        }
+    }
+}
